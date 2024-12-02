c016305c0eac78bf3962f480c8a4c6b929d734ba
Phil Howard <github@gadgetoid.com>
2024-12-02 15:56:15 +0000

    Initial commit.

---
+++ boards/usermod-common.cmake
@@ -0,0 +1,70 @@
+if(NOT DEFINED PIMORONI_PICO_PATH)
+set(PIMORONI_PICO_PATH ${CMAKE_CURRENT_LIST_DIR}/../pimoroni-pico)
+endif()
+include(${PIMORONI_PICO_PATH}/pimoroni_pico_import.cmake)
+
+include_directories(${CMAKE_CURRENT_LIST_DIR}/../../)
+include_directories(${PIMORONI_PICO_PATH}/micropython)
+
+list(APPEND CMAKE_MODULE_PATH "${PIMORONI_PICO_PATH}/micropython")
+list(APPEND CMAKE_MODULE_PATH "${PIMORONI_PICO_PATH}/micropython/modules")
+
+# Drivers, etc
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../pimoroni-pico")
+# modules_py/modules_py
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../pimoroni-pico/micropython")
+# All regular modules
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../pimoroni-pico/micropython/modules")
+
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_CXX_STANDARD 17)
+
+# Essential
+include(pimoroni_i2c/micropython)
+include(pimoroni_bus/micropython)
+
+# Pico Graphics Essential
+include(hershey_fonts/micropython)
+include(bitmap_fonts/micropython)
+include(picographics/micropython)
+
+# Pico Graphics Extra
+include(pngdec/micropython)
+include(jpegdec/micropython)
+include(qrcode/micropython/micropython)
+
+# Sensors & Breakouts
+include(micropython-common-breakouts)
+include(pcf85063a/micropython)
+
+# Utility
+include(adcfft/micropython)
+include(wakeup/micropython)
+
+# Configure wakeup for Inky Frame
+target_compile_definitions(usermod_wakeup INTERFACE
+    -DWAKEUP_HAS_RTC=1
+    -DWAKEUP_HAS_SHIFT_REGISTER=1
+    -DWAKEUP_PIN_MASK=0b01000100
+    -DWAKEUP_PIN_DIR=0b01000100
+    -DWAKEUP_PIN_VALUE=0b01000100
+)
+
+# LEDs & Matrices
+include(plasma/micropython)
+
+# Servos & Motors
+include(pwm/micropython)
+include(servo/micropython)
+include(encoder/micropython)
+include(motor/micropython)
+
+include(modules_py/modules_py)
+
+copy_module(inky_frame.py)
+
+# C++ Magic Memory
+include(cppmem/micropython)
+
+# Disable build-busting C++ exceptions
+include(micropython-disable-exceptions)
