24e7d5349897689097be44e2dcb3438831b4430d
Hel Gibbons <hel@pimoroni.com>
2025-09-18 12:04:55 +0100

    Examples: Change cats into bears

--- examples/inky_frame_placebear.py
+++ examples/inky_frame_placebear.py
@@ -1,19 +1,20 @@
 import gc
-import uos
-import random
-import machine
-import jpegdec
-import uasyncio
-import sdcard
-import WIFI_CONFIG
 from urllib import urequest
-from network_manager import NetworkManager
-# from picographics import PicoGraphics, DISPLAY_INKY_FRAME as DISPLAY      # 5.7"
-# from picographics import PicoGraphics, DISPLAY_INKY_FRAME_4 as DISPLAY  # 4.0"
-from picographics import PicoGraphics, DISPLAY_INKY_FRAME_7 as DISPLAY  # 7.3"
+
+import inky_helper as ih
+import jpegdec
+import machine
+import sdcard
+import uos
+# from picographics import DISPLAY_INKY_FRAME_4 as DISPLAY  # 4.0"
+# from picographics import DISPLAY_INKY_FRAME as DISPLAY    # 5.7"
+# from picographics import DISPLAY_INKY_FRAME_7 as DISPLAY  # 7.3"
+from picographics import \
+    DISPLAY_INKY_FRAME_SPECTRA_7 as DISPLAY  # 7.3" Spectra
+from picographics import PicoGraphics
 
 """
-random placekitten (from a very small set)
+Download a placebear!
 
 You *must* insert an SD card into Inky Frame!
 We need somewhere to save the jpg for display.
@@ -21,28 +22,27 @@ We need somewhere to save the jpg for display.
 
 gc.collect()  # We're really gonna need that RAM!
 
-
-def status_handler(mode, status, ip):
-    print(mode, status, ip)
-
-
-network_manager = NetworkManager(WIFI_CONFIG.COUNTRY, status_handler=status_handler)
-uasyncio.get_event_loop().run_until_complete(network_manager.client(WIFI_CONFIG.SSID, WIFI_CONFIG.PSK))
-
+# connect to wifi
+try:
+    from secrets import WIFI_PASSWORD, WIFI_SSID
+    ih.network_connect(WIFI_SSID, WIFI_PASSWORD)
+except ImportError:
+    print("Add your WiFi credentials to secrets.py")
 
 graphics = PicoGraphics(DISPLAY)
 
 WIDTH, HEIGHT = graphics.get_bounds()
-FILENAME = "/sd/placekitten.jpg"
-ENDPOINT = "http://placecats.com/{0}/{1}"
-
+FILENAME = "/sd/placebear.jpg"
+ENDPOINT = "https://placebear.com/{0}/{1}.jpg"
+# Note that the API needs to be serving up non-progressive .jpg files to work with jpegdec
 
 sd_spi = machine.SPI(0, sck=machine.Pin(18, machine.Pin.OUT), mosi=machine.Pin(19, machine.Pin.OUT), miso=machine.Pin(16, machine.Pin.OUT))
 sd = sdcard.SDCard(sd_spi, machine.Pin(22))
 uos.mount(sd, "/sd")
 gc.collect()  # Claw back some RAM!
 
-url = ENDPOINT.format(WIDTH, HEIGHT + random.randint(0, 10))
+url = ENDPOINT.format(WIDTH, HEIGHT)
+print(url)
 
 socket = urequest.urlopen(url)
 
